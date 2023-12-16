c74ff61892b5fb3b3c21bc8b14fd6a38d8c4abbd
Tomasz Stefaniuk <41304693+esemku@users.noreply.github.com>
2023-12-16 21:30:59 +0100

    feat: add option to reset password

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,11 +4,18 @@ import {
   loginUserHandler,
   logoutUserHandler,
   refreshAccessTokenHandler,
+  forgotPasswordHandler,
+  resetPasswordHandler,
 } from "../controllers/auth.controller";
 import { deserializeUser } from "../middleware/deserializeUser";
 import { requireUser } from "../middleware/requireUser";
 import { validate } from "../middleware/validate";
-import { loginUserSchema, createUserSchema } from "../schema/user.schema";
+import {
+  loginUserSchema,
+  createUserSchema,
+  forgotPasswordSchema,
+  resetPasswordSchema,
+} from "../schemas/user.schema";
 
 const router = express.Router();
 
@@ -19,5 +26,17 @@ router.post("/login", validate(loginUserSchema), loginUserHandler);
 router.get("/refresh", refreshAccessTokenHandler);
 
 router.get("/logout", deserializeUser, requireUser, logoutUserHandler);
+
+router.post(
+  "/forgotpassword",
+  validate(forgotPasswordSchema),
+  forgotPasswordHandler
+);
+
+router.patch(
+  "/resetpassword/:resetToken",
+  validate(resetPasswordSchema),
+  resetPasswordHandler
+);
 
 export default router;
