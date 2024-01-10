31ab071dec56ffe82ebf65e19cb6bae54ec84a15
Tomasz Stefaniuk <41304693+esemku@users.noreply.github.com>
2024-01-10 12:30:51 +0100

    add origin for production

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,16 @@
+let origin:
+  | "https://take-for-help-eyrqfjk52-tstefaniuk.vercel.app"
+  | "http://localhost:3000";
+
+if (process.env.NODE_ENV === "production") {
+  origin = "https://take-for-help-eyrqfjk52-tstefaniuk.vercel.app";
+} else {
+  origin = "http://localhost:3000";
+}
+
 export default {
   redisCacheExpiresIn: 60,
   refreshTokenExpiresIn: 60,
   accessTokenExpiresIn: 15,
-  origin: "http://localhost:3000",
+  origin,
 };
