9f284b2b6ebae309288d30baff816625712dd61c
Tomasz Stefaniuk <41304693+esemku@users.noreply.github.com>
2024-01-10 10:36:34 +0100

    render deploy improvements

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,22 +1,24 @@
 import { cleanEnv, port, str } from "envalid";
 
 const validateEnv = () => {
-  if (process.env.NODE_ENV === "development") {
-    cleanEnv(process.env, {
-      DATABASE_URL: str(),
+  cleanEnv(process.env, {
+    PORT: port(),
+    NODE_ENV: str(),
 
-      POSTGRES_HOST: str(),
-      POSTGRES_PORT: port(),
-      POSTGRES_USER: str(),
-      POSTGRES_PASSWORD: str(),
-      POSTGRES_DB: str(),
-    });
-  } else {
-    cleanEnv(process.env, {
-      PORT: port(),
-      NODE_ENV: str(),
-    });
-  }
+    JWT_KEY: str(),
+
+    DATABASE_URL: str(),
+    POSTGRES_HOST: str(),
+    POSTGRES_PORT: port(),
+    POSTGRES_USER: str(),
+    POSTGRES_PASSWORD: str(),
+    POSTGRES_DB: str(),
+
+    EMAIL_USER: str(),
+    EMAIL_PASS: str(),
+    EMAIL_HOST: str(),
+    EMAIL_PORT: str(),
+  });
 };
 
 export default validateEnv;
